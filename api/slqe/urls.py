0ed969410841169ee44cb80c714bee6d9f9f827a
Duc Duy <duyddse62881@gmail.com>
2021-03-12 20:32:49 +0700

    fixed comment about url

--- api/slqe/urls.py
+++ api/slqe/urls.py
@@ -4,8 +4,8 @@ from slqe.views import *
 urlpatterns = [
     url(r'^users$', SlqeApi.user_list),
     url(r'^users/(\w+)$', SlqeApi.user_detail),
-    url(r'^upload', SlqeApi.get_image),
-    url(r'^images', SlqeApi.images_list),
-    url(r'^images/^[0-9]*$', SlqeApi.images_detail),
-    url(r'^users/(\w+)$/images', SlqeApi.get_images_by_user),
+    url(r'^upload', SlqeApi.process_image),
+    url(r'^images$', SlqeApi.save_image),
+    url(r'^images/([0-9]+)$', SlqeApi.images_detail),
+    url(r'^users/(\w+)/images$', SlqeApi.get_images_by_user),
 ]
