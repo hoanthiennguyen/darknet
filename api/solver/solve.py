d07abd9968a3f9aa9ce7cf5b25512358334931be
hoanthiennguyen <hoanthien97@gmail.com>
2021-03-16 13:56:15 +0700

    Fix case when upper_bound is included

--- api/solver/solve.py
+++ api/solver/solve.py
@@ -19,10 +19,10 @@ def try_round_root(polynomial, raw_root, n_digits):
         return raw_root
 
 
-def find_root_using_bisection(polynomial, epsilon, lower, upper):
+def find_root_using_bisection(polynomial, epsilon, lower, upper, upper_included=False):
     if abs(polynomial.eval(lower)) <= epsilon:
         return lower
-    if abs(polynomial.eval(upper)) <= epsilon:
+    if abs(polynomial.eval(upper)) <= epsilon and not upper_included:
         return None
     if polynomial.eval(lower) * polynomial.eval(upper) > 0:
         return None
@@ -82,7 +82,7 @@ def solve_from_derivative_roots(polynomial, epsilon, derivative_roots):
 
         upper_bound = get_upper_bound_with_opposite_sign(polynomial, derivative_roots[-1])
         if upper_bound is not None:
-            roots.append(find_root_using_bisection(polynomial, epsilon, derivative_roots[-1], upper_bound))
+            roots.append(find_root_using_bisection(polynomial, epsilon, derivative_roots[-1], upper_bound, upper_included=True))
     else:
         lower_bound = get_lower_bound_with_opposite_sign(polynomial, 0)
         upper_bound = get_upper_bound_with_opposite_sign(polynomial, 0)
@@ -90,7 +90,7 @@ def solve_from_derivative_roots(polynomial, epsilon, derivative_roots):
         if lower_bound is not None:
             roots.append(find_root_using_bisection(polynomial, epsilon, lower_bound, 0))
         if upper_bound is not None:
-            roots.append(find_root_using_bisection(polynomial, epsilon, 0, upper_bound))
+            roots.append(find_root_using_bisection(polynomial, epsilon, 0, upper_bound, upper_included=True))
         
     return list(filter(lambda x: x is not None, roots))
 
@@ -285,4 +285,9 @@ class Tests(unittest.TestCase):
         expression = "x^4/4-x^2/2"
         roots = parse_and_solve_and_round(expression, epsilon)
         expected_roots = [-1.4142, 0, 1.4142]
+        self.assertEqual(roots, expected_roots)
+
+        expression = "3*x^2=0"
+        roots = parse_and_solve_and_round(expression, epsilon)
+        expected_roots = [0]
         self.assertEqual(roots, expected_roots)
